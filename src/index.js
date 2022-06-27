a34f11da7e4a46e3ace4761bf4b17e310095fbae
timofeevmg <timofeevmg@gmail.com>
2022-06-27 17:57:40 +0300

    init commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import '../styles/app.css';
+import '../assets/img/html.png';
+import '../assets/img/css.png';
+import '../assets/img/js.png';
+import '../assets/fonts/Raleway-Regular.ttf';
+import '../assets/fonts/Raleway-Bold.ttf';
